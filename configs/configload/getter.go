7db1d53a677a8234d99f58adba30c6b0441c4573
lifangmoler <lmoler@salesforce.com>
2019-02-11 10:32:22 -0800

    from 0.11.11

--- configs/configload/getter.go
+++ configs/configload/getter.go
@@ -17,6 +17,7 @@ import (
 
 var goGetterDetectors = []getter.Detector{
 	new(getter.GitHubDetector),
+	new(getter.GCSDetector),
 	new(getter.BitBucketDetector),
 	new(getter.S3Detector),
 	new(getter.FileDetector),
@@ -42,6 +43,7 @@ var goGetterDecompressors = map[string]getter.Decompressor{
 
 var goGetterGetters = map[string]getter.Getter{
 	"file":  new(getter.FileGetter),
+	"gcs":   new(getter.GCSGetter),
 	"git":   new(getter.GitGetter),
 	"hg":    new(getter.HgGetter),
 	"s3":    new(getter.S3Getter),
